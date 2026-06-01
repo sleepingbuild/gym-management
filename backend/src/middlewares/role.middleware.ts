d052760fac3d3a5aea6a2210a1e7a86150b19682
Poo <phipham690@gmail.com>
2026-06-01 23:39:34 +0700

    [feat] Phase 2 Issues #4-#7: Authentication & Authorization

--- backend/src/middlewares/role.middleware.ts
+++ backend/src/middlewares/role.middleware.ts
@@ -1,1 +1,18 @@
 
+import { Request, Response, NextFunction } from 'express';
+import { Role } from '@prisma/client';
+import { AppError } from '../utils/errors';
+
+export const authorize = (...roles: Role[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return next(new AppError(401, 'AUTH_006: Not authenticated'));
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError(403, 'Forbidden: insufficient permissions'));
+    }
+
+    next();
+  };
+};
