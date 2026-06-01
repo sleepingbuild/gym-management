ec0c314fd70b4bc973b2bab7c10314ec2efc57d4
Poo <phipham690@gmail.com>
2026-06-01 23:42:36 +0700

    [feat] Phase 2 Issues #4-#7: Authentication & Authorization
    Added functions to generate and verify JWT tokens.

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,1 +1,23 @@
 
+import jwt, { SignOptions } from 'jsonwebtoken';
+import { JWT_CONFIG } from '../config/jwt';
+import { TokenPayload, AuthTokens } from '../types/auth.types';
+
+export const generateTokens = (payload: TokenPayload): AuthTokens => {
+  const accessOptions: SignOptions = {
+    expiresIn: JWT_CONFIG.accessTokenExpiry as SignOptions['expiresIn'],
+  };
+
+  const refreshOptions: SignOptions = {
+    expiresIn: JWT_CONFIG.refreshTokenExpiry as SignOptions['expiresIn'],
+  };
+
+  const accessToken = jwt.sign(payload, JWT_CONFIG.secret, accessOptions);
+  const refreshToken = jwt.sign(payload, JWT_CONFIG.secret, refreshOptions);
+
+  return { accessToken, refreshToken };
+};
+
+export const verifyToken = (token: string): TokenPayload => {
+  return jwt.verify(token, JWT_CONFIG.secret) as TokenPayload;
+};
